f531a7b881fcdb1cd9fee78e6a508af1b562f209
Tres Trantham <tres@trestrantham.com>
2022-03-24 00:01:08 -0400

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,10 +17,15 @@ alias f='fzf'
 alias g='git'
 alias gco='git checkout'
 alias gpr='git pull --rebase'
-alias gcom='git checkout master'
+alias gcom='git checkout main'
 alias gempty='git commit --allow-empty -m "Empty commit"'
-alias gclean='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
+alias gclean='git branch --merged | egrep -v "(^\*|main|master|dev)" | xargs git branch -d'
+alias guncommit='git reset --soft HEAD^'
 
 # Code
 alias d='docker-compose run --rm app'
 alias dmix='docker-compose run --rm app mix'
+
+# Nomics
+alias psqlnomnom='psql "postgres://nomnom:7MYkL3UuMNP3PjWWAHqd6c9s86VM88foJbtWoDCrgJ6hmwNhJa82KcZgCPRMmwhw@127.0.0.1:6432/nomnom"'
+alias sshnomnom='ssh nomnom-nomad'
